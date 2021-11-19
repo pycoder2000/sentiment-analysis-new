46d662e65a93b50a42bd07066eaa79ea39fa220a
“parth.desai” <desaiparth2000@gmail.com>
2021-11-19 19:18:41 +0530

    Major Changes

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, abort
 import pickle
 import numpy as np
 import re
@@ -8,15 +6,102 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 import tweepy
 from tweepy.auth import OAuthHandler
+import sys
+from twython import Twython
+import nltk
+from dictionary import Dictionary
 
 app = Flask(__name__)
 
+APP_KEY = 'PXryGcTC4Jt7azDBOSFo3QrZ4'
+APP_SECRET = 'PeKbIwtTpMBV8XcvB1tfandx5NYlquoiVNejZ5uOQLJ4VQVj4I'
+
+twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+ACCESS_TOKEN = twitter.obtain_access_token()
+
+twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+
+class SentimentScore:
+    def __init__(self, positive_tweets, negative_tweets, neutral_tweets):
+
+        self.positive_tweets = positive_tweets
+        self.negative_tweets = negative_tweets
+        self.neutral_tweets = neutral_tweets
+
+        self.neg = len(negative_tweets)
+        self.pos = len(positive_tweets)
+        self.neut = len(neutral_tweets)
+
+
+
+
+dictionaryN = Dictionary('negative-words.txt')
+
+dictionaryP = Dictionary('positive-words.txt')
+
+def sentiment(tweet):
+
+    negative_score = 0
+    positive_score = 0
+
+    tokenizer = nltk.tokenize.TweetTokenizer()
+    tweet_words = tokenizer.tokenize(tweet)
+
+    for word in tweet_words:
+        negative_score += dictionaryN.check(word)
+
+    for word in tweet_words:
+        positive_score += dictionaryP.check(word)
+
+    if negative_score > positive_score:
+        return 'negative'
+    elif negative_score == positive_score:
+        return 'neutral'
+    else:
+        return 'positive'
+
+    # use dictionary to count negative frequent
+
+def sentiment_analysis(tweets):
+
+    negative_tweets = []
+    positive_tweets = []
+    neutral_tweets = []
+
+    for tweet in tweets:
+
+        res = sentiment(tweet['text'])
+
+        if res == 'negative':
+            negative_tweets.append(tweet['text'])
+        elif res == 'positive':
+            positive_tweets.append(tweet['text'])
+        else:
+            neutral_tweets.append(tweet['text'])
+
+    return SentimentScore(positive_tweets, negative_tweets, neutral_tweets)
+
+@app.route("/newroot", methods=["POST","GET"])
+def newroot():
+
+    if request.method == "POST":
+
+        user_timeline = twitter.get_user_timeline(screen_name=request.form['twitter_username'], count = 100)
+
+        return render_template("resultnew.html",anchor='services', result=sentiment_analysis(user_timeline), username=request.form['twitter_username'])
+    else:
+        return render_template("indexnew.html",anchor='services')
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route('/result')
 @app.route('/home')
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 
 @app.route('/predict', methods=['POST'])
